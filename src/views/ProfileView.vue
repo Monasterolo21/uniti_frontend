b2b7a28bec1cd49567133b5aa0e1709b33cb2c7d
Fabio Ferrero <fabio.ferrero@hotmail.it>
2023-02-15 14:35:30 +0100

    Aggiunta alert ok/error quando si modifica il profilo

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,37 +1,39 @@
 <template>
   <div class="panel">
     <header-component
-      :title="'Profilo di ' + this.user.name"
-      subtitle="Ecco le tue informazioni presenti su UNITI"
-      :background="backgroundUrl"
+        :title="'Profilo di ' + this.user.name"
+        subtitle="Ecco le tue informazioni presenti su UNITI"
+        :background="backgroundUrl"
     />
     <p>Puoi modificare le tue informazioni cliccandoci sopra, cambiandone il valore, e cliccando il pulsante
-      <span class="fb">SALVA</span>
+        <span class="fb">SALVA</span>
     </p>
     <div class="edit-panel">
         <div class="field">
-        <div class="title">Foto:</div>
-        <div class="user-image">
-            <img :src="logoUrl"
-            />
-            <!-- <img v-if="!this.user.urlPicture" src="@/assets/img/person.png"/> -->
-        </div>
-        <img class="edit-icon" src="@/assets/img/edit.png" />
+            <div class="title">Foto:</div>
+            <div class="user-image">
+                <img :src="logoUrl"
+                />
+                <!-- <img v-if="!this.user.urlPicture" src="@/assets/img/person.png"/> -->
+            </div>
+            <img class="edit-icon" src="@/assets/img/edit.png" @click="editUrlPicture" />
         </div>
         <div class="field">
-        <div class="title">Nome:</div>
-        <input
-            class="value"
-            contenteditable="true"
-            type="text"
-            v-model="nome"
-        />
+            <div class="title">Nome:</div>
+                <input
+                    class="value"
+                    contenteditable="true"
+                    maxlength="20"
+                    type="text"
+                    v-model="nome"
+            />
         </div>
         <div class="field">
             <div class="title">Cognome:</div>
             <input
                 class="value"
                 contenteditable="true"
+                maxlength="25"
                 type="text"
                 v-model="cognome"
             />
@@ -40,21 +42,22 @@
         <div style="width: 100%;">
             <div class="field">
                 <div class="title">Bio:</div>
-                <input class="value" contenteditable="true" type="text" v-model="bio" />
+                <textarea class="value" contenteditable="true" type="text" v-model="bio" />
             </div>            
         </div>
 
         <div style="width: 100%;">
-            <div class="field2">
+            <div class="field">
                 <div class="title">Url Foto profilo:</div>
-                <input class="value" contenteditable="true" type="text" :placeholder="picUrl" v-model="urlPicture" />
+                <h3 class="value fbred" style="padding-right: 10px;">{{picUrl}}</h3>
+                <!-- <input id="url" class="value" type="text" :placeholder="picUrl" readonly/> -->
             </div>            
         </div>
       
         <div style="width: 100%;">
             <div class="field">
             <div class="title">Email:</div>
-            <div class="value">{{ this.user.email }}</div>      
+            <div class="value" style="padding-right: 10px;">{{ this.user.email }}</div>      
             </div>
             <p>(Email <span class="fb">NON</span> modificabile)</p>
         </div>
@@ -64,6 +67,13 @@
   </div>
 </template>
 
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  
+ */
+
+
 <script>
 import HeaderComponent from "@/components/HeaderComponent.vue";
 import ButtonComponent from "@/components/ButtonComponent.vue";
@@ -71,65 +81,270 @@ import { mapGetters } from "vuex";
 import store from "@/store";
 
 export default {
-  name: "HomeView",
-  components: { HeaderComponent, ButtonComponent },
 
-  data() {
-    return {
-      nome: "nome",
-      cognome: "cognome",
-      bio: "bio",   
-      urlPicture : "url",   
-    };
-  },
+    name: "HomeView",
+    components: { HeaderComponent, ButtonComponent },
 
-  methods: {
-    save() {
-
-      var newUser = Object.assign({}, this.user);
-      newUser.name = this.nome;
-      newUser.surname = this.cognome;
-      newUser.bio = this.bio;
-      newUser.urlPicture = this.urlPicture;
-      console.log('------------')
-      console.log(this.user)
-      console.log(newUser)
-      console.log('------------')
-      store.dispatch('editUser',newUser);
-    },
-  },
-
-  computed: {
-    ...mapGetters({
-      user: "getUser",
-    }),
-
-    picUrl() {
-      return this.user.urlPicture === null ? 'Nessun URL' : this.user.urlPicture;
+    data() {
+        return {
+            nome: "nome",
+            cognome: "cognome",
+            bio: "bio",   
+            urlPicture : "url",   
+        };
     },
 
-    backgroundUrl() {
-      return this.user.urlPicture !== null ? this.user.urlPicture : 
-      'https://images.unsplash.com/photo-1492681290082-e932832941e6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80'
+    methods: {
+
+        async editUrlPicture() {
+            let newUrl = await this.$swal({
+                position: 'center',
+                input : 'url',
+                inputLabel: 'URL address',
+                showConfirmButton: true,
+                showCancelButton: true,
+                toast : false, 
+                confirmButtonColor: '#E39A94',
+            });
+            console.log(newUrl);
+            if(newUrl.isConfirmed) {
+                this.urlPicture = newUrl.value;
+                this.save();
+            }
+        },
+
+        resetUserInfo() {
+            this.nome = this.user.name;
+            this.cognome = this.user.surname;
+            this.bio = this.user.bio;
+            this.urlPicture = this.user.urlPicture;
+        },
+
+        save() {
+            if(!this.nome || !this.cognome) {
+                this.errorHandler();
+                return;
+            }
+            var newUser = Object.assign({}, this.user);
+            newUser.name = this.nome;
+            newUser.surname = this.cognome;
+            newUser.bio = this.bio;
+            newUser.urlPicture = this.urlPicture;
+            let res = store.dispatch('editUser',newUser);
+            res.then((user) => {
+                if (user)
+                    this.alertSuccess();
+                else
+                    this.errorHandler();
+            })
+        },
+
+        errorHandler() {
+            this.resetUserInfo();
+            this.alertError();
+        },
+
+        alertSuccess() {
+            this.$swal({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Complimenti! Hai modificato il tuo profilo!',
+                showConfirmButton: false,
+                timer: 2000,
+                toast : false, 
+            });
+        },
+
+        alertError() {
+            this.$swal({
+                position: 'top-end',
+                icon: 'error',
+                title: 'Qualcosa è andato storto! Ricorda che il nome ed il cognome sono obbligatori!',
+                showConfirmButton: false,
+                timer: 2000,
+                toast : false, 
+            });
+        }
     },
 
-    logoUrl() {
-      return this.user.urlPicture === null ? require('@/assets/img/person0.png') : this.user.urlPicture;
-    }
-  },
+    computed: {
+        ...mapGetters({
+            user: "getUser",
+        }),
+
+        picUrl() {
+            return this.user.urlPicture === null ? 'Nessun URL' : 'La tua immagine è impostata';
+            //return this.user.urlPicture === null ? 'Nessun URL' : this.user.urlPicture;
+        },
+
+        backgroundUrl() {
+            return this.user.urlPicture !== null ? this.user.urlPicture : 
+            'https://images.unsplash.com/photo-1492681290082-e932832941e6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80'
+        },
+
+        logoUrl() {
+            return this.user.urlPicture === null ? require('@/assets/img/person0.png') : this.urlPicture;
+        }
+    },
 
 
-  mounted() {
-    this.nome = this.user.name;
-    this.cognome = this.user.surname;
-    this.bio = this.user.bio;
-    this.urlPicture = this.user.urlPicture;
-  },
+    mounted() {
+        this.nome = this.user.name;
+        this.cognome = this.user.surname;
+        this.bio = this.user.bio;
+        this.urlPicture = this.user.urlPicture;
+    },
 };
 </script>
 
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  
+ */
+
+
 <style>
 .edit-panel {
+  padding: 1em;
+  background: #fff;
+  border-radius: 0.5em;
+  margin: 1em;
+  display: flex;
+  align-items: center;
+  flex-flow: column;
+  margin: 1em auto;
+}
+
+
+.edit-panel .field {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: min(100%, 900px);
+  margin: 1em auto;
+  width: 100%;
+  padding-right: 5%;
+}
+
+.edit-panel .field input {
+    border-radius: 0.5em;
+    background-color: aliceblue;
+    margin-right: 1%;
+    padding-right: 10px;
+}
+
+.edit-panel .field textarea {
+    resize: none;
+    height: 100px;
+    border-radius: 0.5em;
+    background-color: aliceblue;
+    padding-right: 10px;
+    margin-right: 1%;
+    font-family: 'Raleway', sans-serif;
+}
+
+.edit-panel .field select {
+    border-radius: 0.5em;
+    background-color: aliceblue;
+    padding-right: 10px;
+    margin-right: 1%;
+    font-family: 'Raleway', sans-serif;
+}
+
+.edit-panel .field2 {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  max-width: min(100%, 900px);
+  margin: 1em auto;
+  width: 100%;
+}
+
+.edit-panel .field .title {
+  font-size: 1.4em;
+  font-weight: 600;
+  width: 20%;
+  color: var(--primary-color);
+  text-align: left;
+}
+
+.edit-panel .field2 .title {
+  font-size: 1.4em;
+  font-weight: 600;
+  color: var(--primary-color);
+  text-align: left;
+}
+.edit-panel .field .value {
+  font-size: 1.2em;
+  font-weight: 400;
+  width: 100%;
+  color: var(--text-color);
+  border: none;
+  text-align: right;
+  float: right;
+}
+.edit-panel .field2 .value {
+  font-size: 1.2em;
+  font-weight: 400;
+  width: 100%;
+  color: var(--text-color);
+  border: none;
+  text-align: right;
+  float: right;
+}
+.edit-panel .field .edit-icon {
+  width: 1em;
+  height: 1em;
+}
+
+.edit-icon:hover {
+    cursor: pointer;
+}
+
+.fb {
+  font-weight: bold !important
+}
+.fbred {
+    font-weight: bold !important;
+    color: var(--primary-color) !important;
+}
+
+.edit-panel .user-image {
+  width: 3em;
+  height: 3em;
+  border-radius: 5em;
+  overflow: hidden;
+  aspect-ratio: 1/1;
+}
+
+
+.edit-panel .user-image img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+@media screen and (max-width: 1000px) {
+    .edit-panel {
+        margin-left: 1%;
+    }
+
+    .edit-panel .field {
+        display: block;
+    }
+    .edit-panel .field .value {
+        width: 90%;
+        /* margin-left: 5%;
+        margin-right: 5%; */
+        float: left;
+    }
+}
+
+
+/* .edit-panel {
   padding: 1em;
   background: #fff;
   border-radius: 0.5em;
@@ -145,6 +360,16 @@ export default {
   border-radius: 5em;
   overflow: hidden;
   aspect-ratio: 1/1;
+}
+
+.edit-panel .field textarea {
+    resize: none;
+    height: 100px;
+    border-radius: 0.5em;
+    background-color: aliceblue;
+    padding-right: 10px;
+    margin-right: 1%;
+    font-family: 'Raleway', sans-serif;
 }
 
 .edit-panel .user-image img {
@@ -209,5 +434,5 @@ export default {
 }
 .fb {
   font-weight: bold;
-}
+} */
 </style>
